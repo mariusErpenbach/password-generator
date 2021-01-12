e2bacc236bcc024272f12f4264bf4688e97941bb
Hawty1 <49696027+Hawty1@users.noreply.github.com>
2021-01-12 14:24:36 +0100

    created a simple password generator with 30 random letters/numbers

---
+++ password_generator.js
@@ -0,0 +1,10 @@
+function generatePassword(x) {
+    var length = x; //exchange x with a static number to create a static generatorbutton
+        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+        password = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+        password += charset.charAt(Math.floor(Math.random() * n));
+    }
+    return password;
+}
+console.log(generatePassword(30));
