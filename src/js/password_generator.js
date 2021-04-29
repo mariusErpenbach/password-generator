3a58057c1ff2a267089ff3fc3bec8f6d7673acb8
Hawty1 <marius_erpenbach@web.de>
2021-04-29 03:44:46 +0200

    display generated password

--- src/js/password_generator.js
+++ src/js/password_generator.js
@@ -1,10 +1,12 @@
-function generatePassword(x) {
+function generatePassword() {
+    x= document.getElementById("userLength").value;
     var length = x; //exchange x with a static number to create a static generatorbutton
         charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
         password = "";
     for (var i = 0, n = charset.length; i < length; ++i) { // for loop with a second variable to acess the charset array 
         password += charset.charAt(Math.floor(Math.random() * n));
     }
-    // return password;
-    console.log(password)
+    document.getElementById("result").innerHTML=password;
 }
+
+
